5936193b450ff119f5b6d12ed5dc19301801151b
Quentin <quentin.labernia@insa-lyon.fr>
2016-03-30 09:59:04 +0200

    11_wordToVecPython ok

--- out/README.md
+++ out/README.md
@@ -1,7 +1,6 @@
 # Fichiers et données de sorties
 
-## vecteurs_[java|python]_#1.csv
+## 1x_vecteurs_[java|python]_#1.csv
 Ce fichier contient les vecteurs résultants de l'application de
 wordToVec sur les données Twitter (texte uniquement).
-L'argument #1 correspond à la méthode d'agrégation utilisée pour passer des vecteurs
-de mots aux vecteurs de tweet.
+L'argument #1 correspond au numéro associé à l'expérience en cours.
--- workflow/11_wordToVecPython/main.py
+++ workflow/11_wordToVecPython/main.py
@@ -14,8 +14,41 @@ import os
 
 from collections import Counter
 import math
+import argparse
 
-def get_tweet(nb_docs):
+# Traitement de la ligne de commande
+
+parser = argparse.ArgumentParser(description =
+                                 'Exécute Doc2Vec sur la base de Tweet')
+
+
+exc_group = parser.add_mutually_exclusive_group()
+exc_group.add_argument('-m', '--mean', help = 'Moyenne les vecteurs de mots',
+                       action = 'store_true')
+exc_group.add_argument('-s', '--sum', help = 'Somme les vecteurs de mots',
+                       action = 'store_true')
+exc_group.add_argument('-t', '--tfidf', help = 'Effectue la somme pondérée (tfidf) des vecteurs de mots',
+                       action = 'store_true')
+
+parser.add_argument('-d', '--dim', help = 'Dimension des vecteurs de mots',
+                        action ='store', default = 2, type = int)
+
+parser.add_argument('-c', '--coeff', help = 'Associe un coefficient aux hashtags',
+                        action ='store', default = 1, type = int)
+
+parser.add_argument('-a', '--amount', help = 'Nombre de tweet à considérer (0 prend toute la base)',
+                        action ='store', default = 0, type = int)
+
+parser.add_argument('-f', '--filename', help = 'Nom de fichier en sortie',
+                        action ='store', default = None)
+
+parser.add_argument('-n', '--number', help = 'Numéro associé à l\'experience courante',
+                        action ='store', default = 0, type = int)
+
+
+args = parser.parse_args()
+
+def get_tweet():
     print("-> Récupération des tweets...")
     client = MongoClient()
     
@@ -28,16 +61,14 @@ def get_tweet(nb_docs):
     else:
         cursor_tweet = col_twitter.find({},{'text':1, '_id':1})
     
+    if args.amount > 0:
+        cursor_tweet.limit(args.amount)
 
     label_tweet = []
     text_tweet = []
-    geo_tweet = []
-    userid_tweet = []
     
     i = 0
     for tweet in cursor_tweet:
-        if nb_docs != 0 and i >= nb_docs:
-            break
         i = i + 1
 
         label_tweet.append(tweet['_id'])
@@ -49,6 +80,9 @@ def get_tweet(nb_docs):
     print("    ok!")
 
     return label_tweet, text_tweet
+
+def is_hashtag(word):
+    return True if word[0] == '#' else False
 
 def do_tfidf(text_tweet, model):
     # text_tweet est une collection de documents
@@ -95,17 +129,53 @@ def do_tfidf(text_tweet, model):
         bag = tweet.split()
         bag_len = len(bag)
         
-        count = Counter(bag)
-        
-        weighted_vector = None 
+        count = Counter(bag) 
         
         for word in bag:
             # on calcule le tfidf
             cur_tf = float(count[word]) / bag_len
             cur_tfidf = idf[word] * cur_tf
+            cur_pond = 1
+            
+            if is_hashtag(word):
+                cur_pond = args.coeff
             
             # on fait la somme pondérée des vecteurs de mot
             data[row_counter, :] += cur_tfidf * model[word]
+        
+        row_counter += 1
+    
+    return data
+
+def do_mean(text_tweet, model, mean = False):
+    
+    vector_dim = model.syn0.shape[1]
+    nb_tweet = len(text_tweet)
+    
+    data = np.zeros((nb_tweet, vector_dim))
+    
+    row_counter = 0
+    
+    for tweet in text_tweet:
+        
+        bag = tweet.split()
+        sum_pond = 0
+        
+        for word in bag:
+            cur_pond = 1
+            
+            if is_hashtag(word):
+                cur_pond = args.coeff
+            
+            if mean:
+                sum_pond += cur_pond
+            
+            # on fait la somme pondérée des vecteurs de mot
+            data[row_counter, :] += model[word] * cur_pond
+        
+        if mean:
+            # on divise par la pondération
+            data[row_counter, :] /= float(sum_pond)
         
         row_counter += 1
     
@@ -126,13 +196,13 @@ def do_doc2vec(label_tweet, text_tweet):
 
     model = None
 
-    filename_cache = ('model_nbdocs_' + str(nb_docs) +
-                          '_dim_' + str(vec_dim) +
+    filename_cache = ('model_nbdocs_' + str(args.amount) +
+                          '_dim_' + str(args.dim) +
                           '.doc2vec')
     
     if not os.path.exists(filename_cache):
     
-        model = Doc2Vec(documents, size = vec_dim,
+        model = Doc2Vec(documents, size = args.dim,
                     min_count = 1, workers = 4)
     
         model.save(filename_cache)
@@ -140,31 +210,49 @@ def do_doc2vec(label_tweet, text_tweet):
     else:
         model = Doc2Vec.load(filename_cache)
     
+    data = None
     
-    print("   tfidf...")
-    data = do_tfidf(text_tweet, model)
+    if args.coeff != 1:
+        print("    pondération des #tags : " + str(args.coeff))
     
-    mat_docs = np.vstack(model.docvecs)
+    if args.tfidf:
+        print("    tfidf...")
+        data = do_tfidf(text_tweet, model)
+    elif args.mean:
+        print("    mean...")
+        data = do_mean(text_tweet, model, True)
+    else:
+        print("    sum...")
+        data = do_mean(text_tweet, model)
     
-    print('   mat_docs.shape = ' + str(mat_docs.shape))
+    
     print("    ok!")
     
-    model = DBSCAN(eps = 0.01, min_samples = 5)
-    model.fit(mat_docs)
+    # rassembler les labels de chaque tweet
+    # avec les vecteurs correspondants
     
-    label_doc = model.labels_
-    print("doc : " + str(len(set(model.labels_))))
-    print("Clustering doc ok !")
+    data = pd.DataFrame(data)
     
-    return label_doc
+    final_data = pd.DataFrame({'id' : label_tweet})
+    final_data = pd.concat([final_data, data], axis = 1)
+    
+    return final_data
 
 
-nb_docs = 1000
-vec_dim = 2
+label_tweet, text_tweet = get_tweet()
+data = do_doc2vec(label_tweet, text_tweet)
 
-label_tweet, text_tweet = get_tweet(0)
+# sauvegarder le fichier
 
-nb_docs = len(label_tweet)
+prefix = '../../out/'
 
-label_doc = do_doc2vec(label_tweet, text_tweet)
-
+if args.filename is not None:
+    nom = args.filename
+    ext = '.csv'
+    
+    if args.filename[-4:] == '.csv':
+        nom = args.filename[:-4]
+        
+    data.to_csv(prefix + nom + '_' + str(args.number) + ext)
+else:
+    data.to_csv(prefix + '1x_vecteurs_python_' + str(args.number) + '.csv')
